a5ea1c7104a6ce7a1f149e5b6567bdc92011eacc
Anuj Khairnar <anujkhairnar5@gmail.com>
2021-06-12 11:20:50 +0530

    Using set functions

--- process_credentials/uid.c
+++ process_credentials/uid.c
@@ -10,13 +10,40 @@ int main()
 	printf("euid is %d\n", geteuid());
 	printf("gid is %d\n", getgid());
 	printf("egid is %d\n", getegid());
-
-	//we are setting uid here which requiers sudo previledges. setuid returns 0 on success and -1 on error
+	
+	
+	//we are setting uid here which requires sudo previledges. setuid returns 0 on success and -1 on error
 	if(setuid(250) < 0)
 	{
 		perror("setuid ");
 		exit(EXIT_FAILURE);
 	}
 	printf("uid after setuid is %d\n", getuid());
-	return 0;	
+
+	//we are setting euid here which requires sudo previledges. seteuid returns 0 on success and -1 on error
+	if(seteuid(250) < 0)
+	{
+		perror("seteuid ");
+		exit(EXIT_FAILURE);
+	}
+	printf("euid after seteuid is %d\n", geteuid());
+
+
+	//we are setting gid here. setgid returns 0 on success and -1 on error
+        if(setgid(1000) < 0)
+        {
+                perror("setgid ");
+                exit(EXIT_FAILURE);
+        }
+        printf("gid after setgid is %d\n", getgid());
+
+	//we are setting egid. setegid returns 0 on success and -1 on error
+        if(setegid(1000) < 0)
+        {
+                perror("setegid ");
+                exit(EXIT_FAILURE);
+        }
+        printf("egid after setegid is %d\n", getegid());
+	
+	return 0;
 }
